677a688476822c641cd1996beb9420925bd3ede5
sammtcbn <sammtcbn@gmail.com>
2021-11-26 17:39:03 +0800

    linux install script

---
+++ linux_install.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+SERVICE_NAME=mongodb.service
+INS_PATH=/usr/local/sammtcbn/mongodb
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+mkdir -p ${INS_PATH} || exit 1
+cp -a -r -f mongodb/* ${INS_PATH} || exit 1
+cp -a -f ${SERVICE_NAME} /etc/systemd/system || exit 1
+
+mkdir -p ${INS_PATH}/data/db || exit 1
+
+systemctl daemon-reload || exit 1
+chmod 664 /etc/systemd/system/${SERVICE_NAME} || exit 1
+systemctl enable ${SERVICE_NAME} || exit 1
+systemctl start ${SERVICE_NAME} || exit 1
